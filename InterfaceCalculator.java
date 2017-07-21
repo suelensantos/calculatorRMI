fd3f6287ab3c1613e25ad3c373a3141b07c50a5e
Suelen Santos <suelen.santos@corp.globo.com>
2017-07-21 14:25:40 -0300

    Corrigindo identação

--- InterfaceCalculator.java
+++ InterfaceCalculator.java
@@ -7,8 +7,8 @@ import java.rmi.RemoteException;
 
 public interface InterfaceCalculator extends Remote {
 
-	public double somar(double a, double b) throws RemoteException;
-	public double subtrair(double a, double b) throws RemoteException;
+    public double somar(double a, double b) throws RemoteException;
+    public double subtrair(double a, double b) throws RemoteException;
     public double multiplicar(double a, double b) throws RemoteException;
     public double dividir(double a, double b) throws RemoteException, DivisaoPorZeroException;
 }
